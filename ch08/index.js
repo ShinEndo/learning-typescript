82a54b01fe272e401b63d6e9d485377b14376bec
Shin Endo <end3mame@gmail.com>
2023-08-14 15:23:56 +0900

    第8章　クラス

--- ch08/index.js
+++ ch08/index.js
@@ -1,2 +1,20 @@
 // 8　クラス
 // *************************************************
+var Greeter = /** @class */ (function () {
+    function Greeter() {
+    }
+    Greeter.prototype.greet = function (name) {
+        console.log("".concat(name, ", do your stuff!"));
+    };
+    return Greeter;
+}());
+new Greeter().greet("Miss Frizzle");
+// new Greeter().greet(); <- エラーになる
+var Greeted = /** @class */ (function () {
+    function Greeted(message) {
+        console.log("As I always say: ".concat(message));
+    }
+    return Greeted;
+}());
+new Greeted("take chances, make mistakes, get messy");
+// new Greeted(); <- エラーになる
--- ch08/index.ts
+++ ch08/index.ts
@@ -1,2 +1,22 @@
 // 8　クラス
 // *************************************************
+class Greeter {
+    greet(name:string) {
+        console.log(`${name}, do your stuff!`)
+    }
+}
+
+new Greeter().greet("Miss Frizzle");
+
+// new Greeter().greet(); <- エラーになる
+
+class Greeted {
+    constructor(message: string) {
+        console.log(`As I always say: ${message}`);
+    }
+}
+
+new Greeted("take chances, make mistakes, get messy");
+
+// new Greeted(); <- エラーになる
+
