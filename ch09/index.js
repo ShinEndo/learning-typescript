8c31b00585900e8c9d2755e999f4dc34f8d17a5e
Shin Endo <end3mame@gmail.com>
2023-08-15 15:52:07 +0900

    第9章　型修飾子 - 再び any

--- ch09/index.js
+++ ch09/index.js
@@ -1,2 +1,16 @@
 // 9　型修飾子
 // *************************************************
+// 9.1　トップ型
+// *************************************************
+// 9.1.1　再び any
+// *************************************************
+var anyValue;
+anyValue = "lucille Ball";
+anyValue = 123;
+console.log(anyValue);
+function greetComedian(name) {
+    console.log("Announcing ".concat(name.toUpperCase(), "!"));
+}
+greetComedian('Bea Arthur');
+// TypeScriptで型エラーにならないが、JavaScriptで実行するとクラッシュする
+// greetComedian({name: "Bea Arthur"});
--- ch09/index.ts
+++ ch09/index.ts
@@ -1,2 +1,20 @@
 // 9　型修飾子
 // *************************************************
+// 9.1　トップ型
+// *************************************************
+// 9.1.1　再び any
+// *************************************************
+let anyValue: any;
+anyValue = "lucille Ball";
+anyValue = 123;
+console.log(anyValue);
+
+function greetComedian(name: any) {
+    console.log(`Announcing ${name.toUpperCase()}!`);
+}
+
+greetComedian('Bea Arthur');
+
+// TypeScriptで型エラーにならないが、JavaScriptで実行するとクラッシュする
+// greetComedian({name: "Bea Arthur"});
+
