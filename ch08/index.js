ed609054c18b0ef19e90a5d4f64fad4987d2bd94
Shin Endo <end3mame@gmail.com>
2023-08-15 14:25:40 +0900

    第8章　クラス - クラスの拡張

--- ch08/index.js
+++ ch08/index.js
@@ -1,3 +1,18 @@
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        if (typeof b !== "function" && b !== null)
+            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
 var _a;
 // 8　クラス
 // *************************************************
@@ -184,3 +199,27 @@ var ReportCard = /** @class */ (function () {
 // class NotAsNumber implements AgeIsNumber,AgeIsNotNumber {
 //     age(){return "";}
 // } <- エラーになる
+// 8.5　クラスの拡張
+// *************************************************
+var Teacher2 = /** @class */ (function () {
+    function Teacher2() {
+    }
+    Teacher2.prototype.teach = function () {
+        console.log("The surest test of discipline is its absence");
+    };
+    return Teacher2;
+}());
+var StudentTeacher = /** @class */ (function (_super) {
+    __extends(StudentTeacher, _super);
+    function StudentTeacher() {
+        return _super !== null && _super.apply(this, arguments) || this;
+    }
+    StudentTeacher.prototype.learn = function () {
+        console.log("I cannot afford the luxury of a closed mind.");
+    };
+    return StudentTeacher;
+}(Teacher2));
+var teacher2 = new StudentTeacher();
+teacher2.teach();
+teacher2.learn();
+// teacher2.other(); <- エラーになる
--- ch08/index.ts
+++ ch08/index.ts
@@ -246,3 +246,22 @@ interface AgeIsNotNumber {
 // class NotAsNumber implements AgeIsNumber,AgeIsNotNumber {
 //     age(){return "";}
 // } <- エラーになる
+
+// 8.5　クラスの拡張
+// *************************************************
+class Teacher2 {
+    teach() {
+        console.log("The surest test of discipline is its absence");
+    }
+}
+
+class StudentTeacher extends Teacher2 {
+    learn() {
+        console.log("I cannot afford the luxury of a closed mind.");
+    }
+}
+
+const teacher2 =  new StudentTeacher();
+teacher2.teach();
+teacher2.learn();
+// teacher2.other(); <- エラーになる
