240cc047df3c8970f5c8f2fb6bea7dd347b51f04
ShinEndo <endo@elplanning.co.jp>
2023-08-17 08:50:37 +0900

    第10章　ジェネリック - ジェネリック関数

--- ch10/index.js
+++ ch10/index.js
@@ -1,2 +1,17 @@
 // 10　ジェネリック
 // *************************************************
+function identity(input) {
+    return input;
+}
+var value = identity("abc");
+var value2 = identity(123);
+var value3 = identity({ quote: "I think your self emerges more clearly over time." });
+// 10.1　ジェネリック関数
+// *************************************************
+function identity2(input) {
+    return input;
+}
+var stringy = identity2("me");
+var numeric = identity2(123);
+var identity3 = function (input) { return input; };
+var value4 = identity3(123);
--- ch10/index.ts
+++ ch10/index.ts
@@ -1,2 +1,23 @@
 // 10　ジェネリック
 // *************************************************
+function identity(input) {
+    return input;
+}
+
+const value = identity("abc");
+const value2 = identity(123);
+const value3 = identity({quote: "I think your self emerges more clearly over time."});
+
+
+// 10.1　ジェネリック関数
+// *************************************************
+function identity2<T>(input:T) {
+    return input;
+}
+
+const stringy = identity2("me");
+const numeric = identity2(123);
+
+const identity3 = <T>(input:T) => input;
+const value4 = identity3(123);
+
