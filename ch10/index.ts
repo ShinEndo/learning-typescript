f5078c28704953b1220886c17ca7e534bfd2340c
Shin Endo <end3mame@gmail.com>
2023-08-20 22:47:34 +0900

    第10章　ジェネリック - Promise

--- ch10/index.ts
+++ ch10/index.ts
@@ -406,3 +406,48 @@ function get2<T>(container: T, key: keyof T) {
 
 const found = get2(roles,"favorite");
 
+// 10.7　Promise
+// *************************************************
+// 10.7.1　Promiseの作成
+// *************************************************
+class PromiseLike10<Value> {
+    constructor(executor: (resolve: (value: Value) => void, reject: (reason: unknown) => void)=> void) {}
+}
+
+// 型：Promise<unknown>
+const resolvesUnknown = new Promise(resolve => {
+    setTimeout(()=>resolve("Done!"),1000);
+});
+// 型：Promise<string>
+const resolvesString = new Promise<string>(resolve => {
+    setTimeout(()=>resolve("Done!"),1000);
+});
+
+// 型：Promise<string>
+const textEventually = new Promise<string>(resolve => {
+    setTimeout(()=>resolve("Done!"),1000);
+});
+
+// 型：Promise<numbr>
+const numberEventually = textEventually.then(text => text.length);
+
+// 10.7.2　async関数
+// *************************************************
+// 型：(text:string) => Promise<number>
+async function lengthAfterSecond(text:string) {
+    await new Promise(resolve => setTimeout(resolve,1000));
+    return text.length;
+}
+
+// 型：(text:string) => Promise<number>
+async function lengthImmediately(text: string) {
+    return text.length;
+}
+
+async function givesPromiseForString(): Promise<string> {
+    return "Done!";
+}
+
+// async function givesString(): string {
+//     return "Done!"; 
+// }
