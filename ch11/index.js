f4e5a78e28f05185e4f8de54497a98de478f0794
ShinEndo <endo@elplanning.co.jp>
2023-08-22 08:55:28 +0900

    第11章　宣言ファイル - パッケージの型

--- ch11/index.js
+++ ch11/index.js
@@ -1,18 +1,11 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.logWindowVersion = exports.character = void 0;
+exports.greet = exports.logLines = exports.character = void 0;
 exports.character = {
     catchphrase: "Yee-haw!",
     name: "Sandy Cheeks",
 };
 console.log(myGlobalValue);
-// 11.2.2　グローバルなインターフェースのマージ
-// *************************************************
-function logWindowVersion() {
-    // console.log(`Window version is: ${window.myVersion}`);
-    window.alert("Built-in window types still work! Hooray!");
-}
-exports.logWindowVersion = logWindowVersion;
 var globals_1 = require("./types/globals");
 function logData(data) {
     console.log("Data version is ".concat(data.version));
@@ -21,3 +14,25 @@ function logData(data) {
 //logData("aaa");
 logData(globallyDeclared);
 logData(globals_1.locallyDeclared);
+// 11.3　組み込み宣言
+// *************************************************
+// 11.3.1　ライブラリー宣言
+// *************************************************
+function logLines(lines) {
+    lines.forEach(function () { });
+}
+exports.logLines = logLines;
+// 11.4 モジュール宣言
+// *************************************************
+// import { value } from "my-example-lib";
+// console.log(value);
+// 11.5 パッケージの型
+// *************************************************
+// 11.5.1 declaration
+// *************************************************
+var greet = function (text) {
+    console.log("Hello, ".concat(text));
+};
+exports.greet = greet;
+// 11.5.1 declaration
+// *************************************************
--- ch11/index.ts
+++ ch11/index.ts
@@ -5,8 +5,8 @@
 import { Character } from "./types";
 
 export const character: Character = {
-    catchphrase: "Yee-haw!",
-    name: "Sandy Cheeks",
+  catchphrase: "Yee-haw!",
+  name: "Sandy Cheeks",
 };
 
 // 11.2　ランタイム値の宣言
@@ -17,8 +17,8 @@ console.log(myGlobalValue);
 // 11.2.2　グローバルなインターフェースのマージ
 // *************************************************
 export function logWindowVersion() {
-    console.log(`Window version is: ${window.myVersion}`);
-    window.alert("Built-in window types still work! Hooray!");
+  console.log(`Window version is: ${window.myVersion}`);
+  window.alert("Built-in window types still work! Hooray!");
 }
 
 // 11.2.3　グローバルな拡張
@@ -27,7 +27,7 @@ import { Data } from "./types/data";
 import { locallyDeclared } from "./types/globals";
 
 function logData(data: Data) {
-    console.log(`Data version is ${data.version}`);
+  console.log(`Data version is ${data.version}`);
 }
 
 // 型引数がData型じゃないのでエラーになる
@@ -40,12 +40,24 @@ logData(locallyDeclared);
 // *************************************************
 // 11.3.1　ライブラリー宣言
 // *************************************************
-export function logLines(lines: string[]){
-    lines.forEach(()=>{});
+export function logLines(lines: string[]) {
+  lines.forEach(() => {});
 }
 
 // 11.4 モジュール宣言
 // *************************************************
-import {value} from "my-example-lib";
+import { value } from "my-example-lib";
 console.log(value);
 
+// 11.5 パッケージの型
+// *************************************************
+// 11.5.1 declaration
+// *************************************************
+export const greet = (text: string) => {
+  console.log(`Hello, ${text}`);
+};
+
+// 11.5.2 依存パッケージの型
+// *************************************************
+// 11.5.3 パッケージの型を公開する
+// *************************************************
