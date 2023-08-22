2c552053666bdb5081d751355e8f2edfc6cc8903
ShinEndo <endo@elplanning.co.jp>
2023-08-22 09:01:59 +0900

    第11章　宣言ファイル - Definitely Typed

--- ch11/index.js
+++ ch11/index.js
@@ -1,11 +1,18 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.greet = exports.logLines = exports.character = void 0;
+exports.greet = exports.logLines = exports.logWindowVersion = exports.character = void 0;
 exports.character = {
     catchphrase: "Yee-haw!",
     name: "Sandy Cheeks",
 };
 console.log(myGlobalValue);
+// 11.2.2　グローバルなインターフェースのマージ
+// *************************************************
+function logWindowVersion() {
+    console.log("Window version is: ".concat(window.myVersion));
+    window.alert("Built-in window types still work! Hooray!");
+}
+exports.logWindowVersion = logWindowVersion;
 var globals_1 = require("./types/globals");
 function logData(data) {
     console.log("Data version is ".concat(data.version));
@@ -24,8 +31,8 @@ function logLines(lines) {
 exports.logLines = logLines;
 // 11.4 モジュール宣言
 // *************************************************
-// import { value } from "my-example-lib";
-// console.log(value);
+var my_example_lib_1 = require("my-example-lib");
+console.log(my_example_lib_1.value);
 // 11.5 パッケージの型
 // *************************************************
 // 11.5.1 declaration
@@ -34,5 +41,11 @@ var greet = function (text) {
     console.log("Hello, ".concat(text));
 };
 exports.greet = greet;
-// 11.5.1 declaration
+// 11.5.2 依存パッケージの型
+// *************************************************
+// 11.5.3 パッケージの型を公開する
+// *************************************************
+// 11.6 Definitely Typed
+// *************************************************
+// 11.6.1 型の入手可能性
 // *************************************************
