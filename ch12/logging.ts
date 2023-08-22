83236abbb0b5fa904cd504bb11de4b950de3d4a6
ShinEndo <endo@elplanning.co.jp>
2023-08-23 07:40:07 +0900

    第12章　IDE機能の使用 - コードの記述

---
+++ ch12/logging.ts
@@ -0,0 +1,9 @@
+// 12.2.3.1　リネーム
+// *************************************************
+// F2キーでリネームのためのテキストボックスが表示される
+// export function log(...data: unknown[]) {
+//     console.log("[log", ...data);
+//   }
+export function logData(...data: unknown[]) {
+  console.log("[log", ...data);
+}
