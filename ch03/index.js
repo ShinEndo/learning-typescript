39f702daed7fd959f25cf2574ad3278f6bab3db1
ShinEndo <endo@elplanning.co.jp>
2023-08-23 07:47:23 +0900

    第3章　合併型とリテラル型

--- ch03/index.js
+++ ch03/index.js
@@ -1,13 +1,14 @@
+"use strict";
 // 3.1 合併型
 // *************************************************
-var mathematician = Math.random() > 0.5 ? undefined : "Mark Goldberg";
-var thinker = null;
+let mathematician = Math.random() > 0.5 ? undefined : "Mark Goldberg";
+let thinker = null;
 if (Math.random() > 0.5) {
     thinker = "Susanne Langer";
 }
 // 3.1.2 合併型のプロパティ
 // *************************************************
-var physicist = Math.random() > 0.5 ? "Marie Curie" : 84;
+let physicist = Math.random() > 0.5 ? "Marie Curie" : 84;
 physicist.toString();
 // physicist.toUpperCase(); <- エラーになる
 // physicist.toFixed(); <- エラーになる
@@ -15,20 +16,20 @@ physicist.toString();
 // *************************************************
 // 3.2.1　割り当てによる絞り込み
 // *************************************************
-var admiral;
+let admiral;
 admiral = "Gracec Hopper";
 admiral.toUpperCase();
 // admiral.toFixed(); <- エラーになる
 // 3.2.2　条件のチェック
 // *************************************************
-var scientist = Math.random() > 0.5 ? "Rosalind Franklin" : 51;
+let scientist = Math.random() > 0.5 ? "Rosalind Franklin" : 51;
 if (scientist === "Rosalind Franklin") {
     scientist.toUpperCase();
 }
 // scientist.toUpperCase();  <- エラーになる
 // 3.2.2　typeofチェック
 // *************************************************
-var researcher = Math.random() > 0.5 ? "Rosalind Franklin" : 51;
+let researcher = Math.random() > 0.5 ? "Rosalind Franklin" : 51;
 if (typeof researcher === "string") {
     researcher.toUpperCase();
 }
@@ -44,16 +45,16 @@ typeof researcher === "string" ? researcher.toUpperCase() : researcher.toFixed()
 // *************************************************
 // 3.4　厳密なnullチェック
 // *************************************************
-var nameMaybe = Math.random() > 0.5 ? "Tony Hoare" : undefined;
+let nameMaybe = Math.random() > 0.5 ? "Tony Hoare" : undefined;
 //nameMaybe.toLowerCase(); <- エラーになる
 // 3.4.2　真偽性による絞り込み
 // *************************************************
-var geneticist = Math.random() > 0.5 ? "Barbara Mcclintock" : undefined;
+let geneticist = Math.random() > 0.5 ? "Barbara Mcclintock" : undefined;
 if (geneticist) {
     geneticist.toUpperCase();
 }
 // geneticist.toUpperCase(); <- エラーになる
-var biologist = Math.random() > 0.5 && "Rachel Carson";
+let biologist = Math.random() > 0.5 && "Rachel Carson";
 if (biologist) {
     biologist;
 }
@@ -62,10 +63,10 @@ else {
 }
 // 3.4.3　初期値を持たない変数
 // *************************************************
-var mathmatician;
+let mathmatician;
 mathematician === null || mathematician === void 0 ? void 0 : mathematician.length;
 mathematician = "Mark Goldberg";
 mathematician.length;
-var rawDataFirst;
-var RawDataSecond;
-var RawDataThird;
+let rawDataFirst;
+let RawDataSecond;
+let RawDataThird;
