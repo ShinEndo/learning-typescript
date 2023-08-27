abb51332fbdbc72427e9ba9136605e4daac2ce83
Shin Endo <end3mame@gmail.com>
2023-08-27 22:57:59 +0900

    第14章　構文の拡張 - enum（列挙型）

--- ch14/index.js
+++ ch14/index.js
@@ -9,7 +9,7 @@ var Engineer = /** @class */ (function () {
     }
     return Engineer;
 }());
-new Engineer("mechanical").area;
+var engineer = new Engineer("mechanical").area;
 var Engineer2 = /** @class */ (function () {
     function Engineer2(area) {
         this.area = area;
@@ -17,4 +17,51 @@ var Engineer2 = /** @class */ (function () {
     }
     return Engineer2;
 }());
-new Engineer2("mechanical").area;
+var engineer2 = new Engineer2("mechanical").area;
+var NamedEngineer = /** @class */ (function () {
+    function NamedEngineer(name, area) {
+        this.area = area;
+        this.fullName = "".concat(name, ", ").concat(area, " engineer");
+    }
+    return NamedEngineer;
+}());
+// 14.2　実験的なデコレーター
+// *************************************************
+// 14.3　enum（列挙型）
+// *************************************************
+var StatusCode;
+(function (StatusCode) {
+    StatusCode[StatusCode["InternalServerError"] = 500] = "InternalServerError";
+    StatusCode[StatusCode["NotFound"] = 404] = "NotFound";
+    StatusCode[StatusCode["Ok"] = 200] = "Ok";
+})(StatusCode || (StatusCode = {}));
+// 14.3.1　自動的な数値
+// *************************************************
+var VisualTheme;
+(function (VisualTheme) {
+    VisualTheme[VisualTheme["Dark"] = 0] = "Dark";
+    VisualTheme[VisualTheme["Light"] = 1] = "Light";
+    VisualTheme[VisualTheme["System"] = 2] = "System";
+})(VisualTheme || (VisualTheme = {}));
+var Direction;
+(function (Direction) {
+    Direction[Direction["Top"] = 1] = "Top";
+    Direction[Direction["Right"] = 2] = "Right";
+    Direction[Direction["Bottom"] = 3] = "Bottom";
+    Direction[Direction["Left"] = 4] = "Left";
+})(Direction || (Direction = {}));
+// 14.3.2　文字列値のenum
+// *************************************************
+var LoadStyle;
+(function (LoadStyle) {
+    LoadStyle["AsNeeded"] = "as-needed";
+    LoadStyle["Eager"] = "eager";
+})(LoadStyle || (LoadStyle = {}));
+var displayHint = 2 /* DisplayHint.Transparent */;
+var DisplayHint2;
+(function (DisplayHint2) {
+    DisplayHint2[DisplayHint2["Opaque"] = 0] = "Opaque";
+    DisplayHint2[DisplayHint2["Semitransparent"] = 1] = "Semitransparent";
+    DisplayHint2[DisplayHint2["Transparent"] = 2] = "Transparent";
+})(DisplayHint2 || (DisplayHint2 = {}));
+var displayHint2 = DisplayHint2.Transparent;
--- ch14/index.ts
+++ ch14/index.ts
@@ -28,3 +28,56 @@ class NamedEngineer {
         this.fullName = `${name}, ${area} engineer`;
     }
 }
+
+// 14.2　実験的なデコレーター
+// *************************************************
+// 14.3　enum（列挙型）
+// *************************************************
+enum StatusCode {
+    InternalServerError = 500,
+    NotFound = 404,
+    Ok = 200,
+}
+
+// 14.3.1　自動的な数値
+// *************************************************
+enum VisualTheme {
+    Dark,
+    Light,
+    System
+}
+
+enum Direction {
+    Top = 1,
+    Right,
+    Bottom,
+    Left,
+}
+
+// 14.3.2　文字列値のenum
+// *************************************************
+enum LoadStyle {
+    AsNeeded = "as-needed",
+    Eager = "eager",
+}
+
+// 14.3.3　const enum（定数列挙型）
+// *************************************************
+const enum DisplayHint {
+    Opaque = 0,
+    Semitransparent,
+    Transparent
+}
+
+let displayHint = DisplayHint.Transparent;
+
+enum DisplayHint2 {
+    Opaque = 0,
+    Semitransparent,
+    Transparent
+}
+
+let displayHint2 = DisplayHint2.Transparent;
+
+
+
