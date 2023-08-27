7f9cd46f52d94c76d0833ee41259fa781f65d39c
Shin Endo <end3mame@gmail.com>
2023-08-27 22:32:45 +0900

    第14章　構文の拡張 - クラスのパラメータープロパティ

--- ch14/index.js
+++ ch14/index.js
@@ -1,2 +1,20 @@
 // 14　構文の拡張
 // *************************************************
+// 14.1　クラスのパラメータープロパティ
+// *************************************************
+var Engineer = /** @class */ (function () {
+    function Engineer(area) {
+        this.area = area;
+        console.log("I work in the ".concat(area, " area"));
+    }
+    return Engineer;
+}());
+new Engineer("mechanical").area;
+var Engineer2 = /** @class */ (function () {
+    function Engineer2(area) {
+        this.area = area;
+        console.log("I work in the ".concat(area, " area"));
+    }
+    return Engineer2;
+}());
+new Engineer2("mechanical").area;
--- ch14/index.ts
+++ ch14/index.ts
@@ -1,2 +1,30 @@
 // 14　構文の拡張
 // *************************************************
+// 14.1　クラスのパラメータープロパティ
+// *************************************************
+class Engineer {
+    readonly area: string;
+
+    constructor(area:string) {
+        this.area = area;
+        console.log(`I work in the ${area} area`);
+    }
+}
+
+const engineer = new Engineer("mechanical").area;
+
+class Engineer2 {
+    constructor(readonly area: string) {
+        console.log(`I work in the ${area} area`);
+    }
+}
+
+const engineer2 = new Engineer2("mechanical").area;
+
+class NamedEngineer {
+    fullName: string;
+
+    constructor(name: string, public area: string) {
+        this.fullName = `${name}, ${area} engineer`;
+    }
+}
